77f9e26af33644ce09b98b87cde59d8fdaaf86b0
dchemishanov <dzhovani.chemishanov@strypes.eu>
2019-01-30 14:52:19 +0200

    import request and handle post data; pseudo code in comments

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import Flask, render_template
+from flask import request
 from engine import rg_of_ur
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('home.html')
+    # init a game class
+    if request.method == 'POST':
+        pass
+        # blue_player_position = request.form['blue']
+        # red_player_position = request.form['red']
+        # term = request.form['term']
+        # load the game instance with the post data
+        # game.init_state(blue_player_position, red_player_position, term)
+    else:
+        pass # no move, the game just starts
+        # delete this part of the logic
+    # game.trow_dices()
+    # template_params = game.state()
+    return render_template('home.html') # add template_params as param to templete renderer
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
